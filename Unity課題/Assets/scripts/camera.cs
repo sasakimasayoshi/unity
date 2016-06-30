5eea094a95f397b3499d9d0c409cdc9133602e25
sasakimasayoshi <atlasia0930@yahoo.co.jp>
2016-06-30 10:22:49 +0900

    unity

---
+++ Unity課題/Assets/scripts/camera.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera : MonoBehaviour {
+
+	private GameObject player = null;
+	private Vector3 offset = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+		offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newPosition = transform.position;
+		newPosition.x = player.transform.position.x + offset.x;
+		newPosition.y = player.transform.position.y + offset.y;
+		newPosition.z = player.transform.position.z + offset.z;
+		transform.position = newPosition;
+	}
+}
